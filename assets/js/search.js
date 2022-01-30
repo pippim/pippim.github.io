24417a83fc74f907e3db32c018c14e5d17f981dd
pippim <92641463+pippim@users.noreply.github.com>
2022-01-30 16:20:43 -0700

    Add search_urls arrat

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,24 @@
 // From: https://stackoverflow.com/a/55784549/6929343
-async function load() {
+async function load_search_objects() {
     let url = 'https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json';
-    let obj = null;
+    let search_words = null;
 
     try {
-        obj = await (await fetch(url)).json();
+        search_words = await (await fetch(url)).json();
     } catch(e) {
-        console.log('error');
+        console.log('error: search_words could not be loaded.');
     }
 
-    console.log(obj["brightness"]);
-}
+    console.log(search_words["brightness"]);
 
-load();
+    let url = 'https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json';
+    let search_urls = null;
+
+    try {
+        search_urls = await (await fetch(url)).json();
+    } catch(e) {
+        console.log('error: search_urls could not be loaded.');
+    }
+
+    console.log(search_urls[1000]);
+}
