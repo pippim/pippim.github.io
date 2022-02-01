ff15467ccc8f775fe2420a34fdcd53ef82544429
pippim <92641463+pippim@users.noreply.github.com>
2022-01-31 18:07:28 -0700

    More global scope tweaking

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,26 +4,26 @@ var search_words = null         // global context
 var search_urls = null           //   "      "
 
 // From: https://stackoverflow.com/a/55784549/6929343
-var search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json')
-console.log(search_words["brightness"]);
-var search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json')
-console.log(search_urls[1000]);
+var search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'display')
+var search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 630)
 
 async function load_search_objects() {
-    // search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json')
-    console.log(search_words["brightness"]);
-    // search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json')
-    console.log(search_urls[1000]);
+    search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'brightness')
+    // console.log(search_words['brightness']);
+    search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 750)
+    // console.log(search_urls[750]);
 }
 
-async function load(url) {
+async function load(url, key_or_index) {
     let obj = null;
 
     try {
         obj = await (await fetch(url)).json();
     } catch(e) {
-        console.log('load error: object could not be fetched.');
+        console.log("load(url, key_or_index) error: 'obj' could not be fetched.");
     }
+
+    console.log(obj[key_or_index]);
 
     return obj
 }
