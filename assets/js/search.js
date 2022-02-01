f5daf63a5c5283bd9c93ad2d9ab8e364c2424bf1
pippim <92641463+pippim@users.noreply.github.com>
2022-01-31 17:26:07 -0700

    Back to old method reading each time

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,11 +4,13 @@ MyLib.search_words = null         // global context
 MyLib.earch_urls = null           //   "      "
 
 // From: https://stackoverflow.com/a/55784549/6929343
-MyLib.search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json')
-MyLib.search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json')
+// MyLib.search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json')
+// MyLib.search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json')
 
 async function load_search_objects() {
+    MyLib.search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json')
     console.log(MyLib.search_words["brightness"]);
+    MyLib.search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json')
     console.log(MyLib.search_urls[1000]);
 }
 
