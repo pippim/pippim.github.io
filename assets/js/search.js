13e16ae408d570ffa8902ab988b8e5585141539e
pippim <92641463+pippim@users.noreply.github.com>
2022-01-31 18:14:39 -0700

    Suppress initial load

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,14 +4,15 @@ var search_words = null         // global context
 var search_urls = null           //   "      "
 
 // From: https://stackoverflow.com/a/55784549/6929343
-var search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'display')
-var search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 630)
+// Below runs OK, but what's the point if it's not saved?
+// var search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'display')
+// var search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 630)
 
 async function load_search_objects() {
     search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'brightness')
-    // console.log(search_words['brightness']);
+    // console.log(search_words['brightness']);  // Undefined because fetch isn't finished?
     search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 750)
-    // console.log(search_urls[750]);
+    // console.log(search_urls[750]);  // Undefined because fetch isn't finished?
 }
 
 async function load(url, key_or_index) {
@@ -23,9 +24,9 @@ async function load(url, key_or_index) {
         console.log("load(url, key_or_index) error: 'obj' could not be fetched.");
     }
 
-    console.log(obj[key_or_index]);
+    console.log(obj[key_or_index]);  // obj[index] Works ok here, but not outside of function?
 
-    return obj
+    return obj  // Are we returning nothing because fetch isn't finished yet?
 }
 
 async function load_search_objects_old() {
