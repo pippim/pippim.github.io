ad0ed1cbdc5be6eaaacb9b2fb0abae92167bae38
pippim <92641463+pippim@users.noreply.github.com>
2022-02-01 17:19:21 -0700

    Take out experimental code

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,16 +5,11 @@ var search_words = null         // global context
 var search_urls = null           //   "      "
 
 // From: https://stackoverflow.com/a/55784549/6929343
-// Below runs OK, but what's the point if it's not saved?
-// var search_words = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'display')
-// var search_urls = load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 630)
+var search_words = await load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json')
+var search_urls  = await load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 630)
 
 async function load_search_objects() {
-    // Promise.all([
-        search_words = await load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_include.json', 'brightness'),
-        search_urls  = await load('https://raw.githubusercontent.com/pippim/pippim.github.io/main/assets/json/search_url.json', 750)
-        search_fetched();
-    // ]).then(() => search_fetched())
+    search_fetched();
 }
 
 function search_fetched() {
@@ -22,16 +17,14 @@ function search_fetched() {
     console.log("search_urls:  " + search_urls[630]);
 }
 
-async function load(url, key_or_index) {
+async function load(url) {
     let obj = null;
 
     try {
         obj = await (await fetch(url)).json();
     } catch(e) {
-        console.log("load(url, key_or_index) error: 'obj' could not be fetched.");
+        console.log("load(url) error: 'obj' could not be fetched.");
     }
-
-    console.log(obj[key_or_index]);
 
     return obj;
 }
