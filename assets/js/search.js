86f46ca87026944409457a1f19e9723d55a6a882
pippim <92641463+pippim@users.noreply.github.com>
2022-02-01 17:56:49 -0700

    Initial version js for search bar submit

--- assets/js/search.js
+++ assets/js/search.js
@@ -27,4 +27,19 @@ async function load(url) {
     return obj;
 }
 
+// From: https://pagedart.com/blog/how-to-add-a-search-bar-in-html/
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'pagedart.com';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
+
 /* End of /assets/js/search.js */
