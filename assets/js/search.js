f30721116a38670502811dabaf1ccf641ca12508
pippim <92641463+pippim@users.noreply.github.com>
2022-02-03 17:44:07 -0700

    result_indices[] not defined

--- assets/js/search.js
+++ assets/js/search.js
@@ -78,7 +78,7 @@ function get_results(submit_str) {
 
     for (const word of words) {
         if (search_include[word] !== null) {
-            result_indices = search_include[word]
+            let result_indices = search_include[word]
             let results = result_indices.split(' ')
             for (const result in results) {
                 results_list.push(result);
