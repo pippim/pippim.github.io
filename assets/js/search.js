7a6025c7a1794ee5c164d9568053b798ca137f38
pippim <92641463+pippim@users.noreply.github.com>
2022-02-04 05:44:13 -0700

    .slice(0, 4) not [0, 4]

--- assets/js/search.js
+++ assets/js/search.js
@@ -73,7 +73,7 @@ function submitted(event) {
     console.log("Number of results: " + results.length);
     const top_summary = sum_and_sort(results, 25);
     console.log("Top 25 results: " + top_summary + " | Top 5 URLs below:");
-    for (url_ndx of top_summary[0:4]) {
+    for (url_ndx of top_summary.slice(0, 4) {
         console.log("url_ndx: " + url_ndx + "URL: " + search_urls[url_ndx]);
     }
     // const url = google + site + '+' + q.value;
