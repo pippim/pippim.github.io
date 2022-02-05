98aad9b8fb72421cf7fa19ceb62a3300a97f1fcf
pippim <92641463+pippim@users.noreply.github.com>
2022-02-05 13:38:38 -0700

    change: modal to: search-modal

--- assets/js/search.js
+++ assets/js/search.js
@@ -150,25 +150,25 @@ function sum_and_sort(raw, top_limit) {
         var modal = document.getElementById('search-modal');
 
         // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+        var btn = document.getElementById("search-button");
 
         // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
 
         // When the user clicks the button, open the modal 
-        btn.onclick = function () {
-            modal.style.display = "block";
+        search-button.onclick = function () {
+            search-modal.style.display = "block";
         }
 
         // When the user clicks on <span> (x), close the modal
         span.onclick = function () {
-            modal.style.display = "none";
+            search-modal.style.display = "none";
         }
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
+            if (event.target == search-modal) {
+                search-modal.style.display = "none";
             }
         }
 
