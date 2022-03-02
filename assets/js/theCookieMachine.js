3e1de9459cda394dd415cf728c3d06483afae912
pippim <92641463+pippim@users.noreply.github.com>
2022-03-01 18:37:29 -0700

    line draw line-height 1.1rem works, so try 1.2rem

--- assets/js/theCookieMachine.js
+++ assets/js/theCookieMachine.js
@@ -140,7 +140,7 @@ function setLineDrawFont(elmnt) {
     // font-family from: _sass/jeckyll-theme-cayman.scss line 36
     elmnt.style.cssText = `
       font-family: Consolas, "Liberation Mono", Menlo, Courier, "Courier New", monospace;
-      line-height: 1.1;
+      line-height: 1.2;
     `;
 }
 
