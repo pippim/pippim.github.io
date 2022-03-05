949b6e78412c87b082736d33d4601d68c2df23c0
pippim <92641463+pippim@users.noreply.github.com>
2022-03-04 18:08:25 -0700

    Was updating wrong file :(

--- assets/js/hyperlinkRecipe.js
+++ assets/js/hyperlinkRecipe.js
@@ -23,6 +23,8 @@
 */
 
 var html = null
+var hrClass = "<style>"
+var hrClass = "<style>"
 
 export function processHyperlinkRecipe(b) {
     // b = TCM Window Body
@@ -30,10 +32,12 @@ export function processHyperlinkRecipe(b) {
     //console.log("b: " + b)
     //const b2 = document.getElementById('tcm_window_body')
     //console.log("b2: " + b2)
-    html = "<p>";
+    paintTable();
+    return
+    html += "<p>";
     html += "<h3>Hyperlink Recipe Baker</h3>\n";
     html += "Ingredients - From clipboard. '*' = Mandatory<br>\n";
-    html += "&emsp;URL (href) *<br>\n";
+    html += "&emsp;href (href) *<br>\n";
     html += "&emsp;Name (text) *<br>\n";
     html += "&emsp;Tooltip (title)<br>\n";
     html += "Options - On/Off<br>\n";
@@ -43,7 +47,36 @@ export function processHyperlinkRecipe(b) {
     html += "&emsp;HTML<br>\n";
     html += "&emsp;Markdown\n";
     html += "</p>";
-    b.innerHTML = html;              // Update TCM Window body
+    b.innerHTML = html;              // Append extra html to TCM Window body
 }
+
+function paintTable () {
+    html = '<table id="hrTable" class="hr_table">\n'
+    html += '<tr>\n'
+    html += '<th>Get from Clipboard</th>\n'
+    html += '<th>Or type/paste manually</th>\n'
+    html += '</tr>\n'
+
+    html += '<tr>\n'
+    html += '<td><button onclick="click_href()">URL (href)</button></td>\n'
+    html += '<td><div id="hrHref" class="hr_href>\n'
+    html += '<input type="string">https://pippim.github.io\n'
+    html += '</div></td>\n'
+    html += '</tr>\n'
+
+    html += '<tr>\n'
+    html += '<td><button onclick="click_text()">Name (text)</button></td>\n'
+    html += '<td><div id="hrHref" class="hr_href>\n'
+    html += '<input type="string">https://pippim.github.io\n'
+    html += '</div></td>\n'
+    html += '</tr>\n'
+
+    html += '</table>'
+    b.innerHTML = html;              // Update TCM Window body
+
+}
+
+function click_href () {}
+function click_text () {}
 
 /* End of /assets/js/hyperlinkRecipe.js */
