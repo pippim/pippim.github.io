63e546f847ef39b155bab2544a30c2e1167a5f6f
pippim <92641463+pippim@users.noreply.github.com>
2022-03-13 12:51:00 -0600

    #tcm_window_body td (

--- assets/js/theCookieMachine.js
+++ assets/js/theCookieMachine.js
@@ -189,7 +189,12 @@ function home_page_to_html(results) {
     html += '#tcm_window_body {\n' +
             '  margin: 0;' +
             '}\n'
-    html += '#tcm_window_body table { border-collapse: collapse ! important; }'
+    // No borders inside the table
+    // html += '#hrb_body table, tr, th, td { border: none ! important; }\n'
+    // Table details: Space between columns
+    // html += '#hrb_body td { padding: 0 1rem; }\n'
+    html += '#tcm_window_body table { border-collapse: collapse ! important; }\n'
+    html += '#tcm_window_body td { padding: 0 1rem; }\n'
     html += '</style>'  // Was extra \n causing empty space at bottom?
     b.innerHTML = html; // Update TCM Window body
 }
