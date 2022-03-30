c49edcca4d2338a4feda17150d3947e64df1acae
pippim <92641463+pippim@users.noreply.github.com>
2022-03-29 20:29:24 -0600

    === undefined

--- assets/js/search.js
+++ assets/js/search.js
@@ -18,12 +18,14 @@ var raw_url = raw_url.replace('/blob/', '/');
 // var search_words = JSON.parse(sessionStorage.search_words);
 // var search_words = sessionStorage.search_words;
 var search_words = JSON.parse(sessionStorage.getItem('search_words'));
-if (typeof search_words === "undefined") { load_search_words(); }
+// if (typeof search_words === "undefined") { load_search_words(); }
+if (search_words === undefined) { load_search_words(); }
 load_search_words();
 // var search_urls = JSON.parse(sessionStorage.search_urls);
 // var search_urls = sessionStorage.search_urls;
 var search_urls = JSON.parse(sessionStorage.getItem('search_urls'));
-if (typeof search_urls === "undefined") { load_search_urls(); }
+// if (typeof search_urls === "undefined") { load_search_urls(); }
+if (search_urls === undefined) { load_search_urls(); }
 
 async function load_search_objects() {
     // TODO: rename search_url.json to search_urls.json
