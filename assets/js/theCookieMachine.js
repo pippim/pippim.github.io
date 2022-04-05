9adc17277cb70e04717945f3027c6f5c716e90b2
pippim <92641463+pippim@users.noreply.github.com>
2022-04-04 18:04:12 -0600

    javascripttutorial.net/web-apis/javascript-notification/

--- assets/js/theCookieMachine.js
+++ assets/js/theCookieMachine.js
@@ -137,8 +137,48 @@ document.querySelector('#tcm_cookie_jar').addEventListener('click', () => {
     // Display webpage info - filename, front matter and text (WIP)
     // raw_url set in search.js loaded before us
     // https://www.javascripttutorial.net/web-apis/javascript-notification/
-    const greeting = new Notification('Multi-Timer is running');
-    setTimeout(() => greeting.close(), 10*1000);
+(async () => {
+    // create and show the notification
+    const showNotification = () => {
+        // create a new notification
+        const notification = new Notification('JavaScript Notification API', {
+            body: 'This is a JavaScript Notification API demo',
+            icon: '{{ site.url }}/assets/img/earth-from-space.jpg'
+        });
+
+        // close the notification after 10 seconds
+        setTimeout(() => {
+            notification.close();
+        }, 10 * 1000);
+
+        // navigate to a URL when clicked
+        notification.addEventListener('click', () => {
+
+            window.open('https://www.javascripttutorial.net/web-apis/javascript-notification/', '_blank');
+        });
+    }
+
+    // show an error message
+    const showError = () => {
+        const error = document.querySelector('.error');
+        error.style.display = 'block';
+        error.textContent = 'You blocked the notifications';
+    }
+
+    // check notification permission
+    let granted = false;
+
+    if (Notification.permission === 'granted') {
+        granted = true;
+    } else if (Notification.permission !== 'denied') {
+        let permission = await Notification.requestPermission();
+        granted = permission === 'granted' ? true : false;
+    }
+
+    // show notification or error
+    granted ? showNotification() : showError();
+
+})();
 });
 
 function introduction_to_html() {
