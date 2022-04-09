be99c0604538a04b6da1b9c52a835c16aa43b123
pippim <92641463+pippim@users.noreply.github.com>
2022-04-09 05:40:52 -0600

    // mozBrightness is undefined

--- _includes/tcm-common-code.js
+++ _includes/tcm-common-code.js
@@ -228,9 +228,9 @@ function htmlScreenInfo() {
     // mozEnabled is undefined, User must enable manually
     //html += '  <tr><td>screen.mozEnabled</td>\n' ;
     //html += '  <td>' + screen.mozEnabled.toLocaleString(); + '</td></tr>\n';
-    // Test single screen property
-    html += '  <tr><td>screen.mozBrightness</td>\n' ;
-    html += '  <td>' + screen.mozBrightness.toLocaleString(); + '</td></tr>\n';
+    // mozBrightness is undefined, User must enable manually
+    //html += '  <tr><td>screen.mozBrightness</td>\n' ;
+    //html += '  <td>' + screen.mozBrightness.toLocaleString(); + '</td></tr>\n';
     // Loop through all properties
     for (const [key, value] of Object.entries(screen)) {
         html += '  <tr><td>' + key + '</td>\n' ;
