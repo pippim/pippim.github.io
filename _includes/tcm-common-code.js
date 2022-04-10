beff5818e97fe5b0f6dfb4b2660124a407288be8
pippim <92641463+pippim@users.noreply.github.com>
2022-04-10 09:30:32 -0600

    if (typeof display === 'undefined')
    typeof always returns a STRING !!!

--- _includes/tcm-common-code.js
+++ _includes/tcm-common-code.js
@@ -229,7 +229,7 @@ function htmlWindowInfo() {
         html += '  <tr><td>' + key + '</td>\n' ;
         html += '  <td>';  // Start of table cell
         var display = value;
-        if (typeof display === undefined) { var display = "Undefined" }
+        if (typeof display === 'undefined') { var display = "Undefined" }
         if (display === null) { display = "Null" };
         display = display.toString();  // Needed for test if startsWith "function"
         // We don't want to list HUGE session storage strings
