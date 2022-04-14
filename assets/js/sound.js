1a3ea540894cd93452ad22b054ac57b8d6d546a6
pippim <92641463+pippim@users.noreply.github.com>
2022-04-14 05:05:51 -0600

    Create fetch_sound()

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -48,6 +48,46 @@ async function load_sound(name) {
           reader.readAsDataURL(blob);
         });
       });
-};
+}
+
+var stockNames = ["Alarm_03.mp3", "Alarm_10.mp3", "Alarm_12.mp3"];
+var stockPrefix = "{{ site.url }}/assets/sound/";
+
+// If stock name isn't in local storage, fetch it from website
+for (var i = 0; i < stockNames.length; i++) {
+    if (localStorage.getItem(stockNames[i]) === undefined) {
+        fetch_sound(stockNames[i]);
+    }
+}
+
+async function fetch_sound(name) {
+    // Get from internet and store in localStorage
+    var audioFileUrl = stockPrefix + name
+    fetch(audioFileUrl)
+      .then((response)=>response.blob())
+      .then((blob)=>{
+
+        var size = blob.size;
+        var type = blob.type;
+        var reader = new FileReader();
+
+        reader.addEventListener("loadend", function() {
+            var base64FileData = reader.result.toString();
+            var mediaFile = {
+              fileUrl: audioFileUrl,
+              size: blob.size,
+              type: blob.type,
+              src: base64FileData
+            };
+            // save the file info to localStorage
+            localStorage.setItem(name, JSON.stringify(mediaFile));
+            console.log('fetch_sound() COMPLETE: ' + audioFileUrl)
+        });
+        // Appears below is executed first, then above runs...
+        reader.readAsDataURL(blob);
+      });
+    console.log('fetch_sound() STARTED: ' + audioFileUrl)
+}
+
 
 /* End of /assets/js/sound.js */
