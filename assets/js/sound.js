7577ec656a3d41981c13a72ba4eeb248367aed0d
pippim <92641463+pippim@users.noreply.github.com>
2022-04-13 18:40:30 -0600

    load alarm_03.mp3  from sound.js

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -7,31 +7,47 @@
 ============================================================================ */
 
 // Session Storage statistics
-var stock_sounds = {}
+var stock_sounds = {}  // FUTURE
 
 // search_stats declared in /assets/js/search.js
+if (localStorage.Alarm_03 === undefined) { load_sound('Alarm_03'); }
+else { var Alarm_03 = JSON.parse(localStorage.getItem('Alarm_03')); }
 
 // Fetch config.yml from internet or session Storage
 
-async function load_sound_file(path) {
-    // Get from internet and store in session
-    fetch(raw_url + path)
-        .then((response)=>response.text())
-        .then((responseJson)=>{
-            config_yml = responseJson;
-            buildConfigYml()
-            sessionStorage.setItem('config_yml', config_yml);
-            search_stats["timeSiteRefreshed"] = timeSiteRefreshed;
-            //search_stats["timeSiteRefreshed"] = 16930518;
-            buildStats('_config.yml Count', arrConfigYml.length);
-            buildStats('_config.yml Size', config_yml.length);
+async function load_sound(name) {
+    // Get from internet and store in localStorage
+    // raw_url defined in /assets/js/search.js
+    var audioFileUrl = raw_url + "/assets/sound/" + name + ".mp3"
+    fetch(audioFileUrl)
+      .then(function(res) {
+        res.blob().then(function(blob) {
+          var size = blob.size;
+          var type = blob.type;
+          var reader = new FileReader();
+
+          reader.addEventListener("loadend", function() {
+            // console.log('reader.result:', reader.result);
+            // 1: play the base64 encoded data directly works
+            // audioControl.src = reader.result;
+            // 2: Serialize the data to localStorage and read it back then play...
+            var base64FileData = reader.result.toString();
+            var mediaFile = {
+              fileUrl: audioFileUrl,
+              size: blob.size,
+              type: blob.type,
+              src: base64FileData
+            };
+            // save the file info to localStorage
+            localStorage.setItem(name, JSON.stringify(mediaFile));
+            var reReadItem = JSON.parse(localStorage.getItem(name));
+            audioControl.src = reReadItem.src;
+          });
+          // Appears below is executed first, then above runs...
+          reader.readAsDataURL(blob);
         });
-}
-
-// From: https://pagedart.com/blog/how-to-add-a-search-bar-in-html/
-function play_sound(event) {
-
-    event.preventDefault();                 // Not sure what this does?
-}
+      });
+  });
+};
 
 /* End of /assets/js/sound.js */
