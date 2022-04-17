50caf0cfd377993874416be7b65754ce84872005
pippim <92641463+pippim@users.noreply.github.com>
2022-04-17 12:33:17 -0600

    ttaProjects was wrong spelling...

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -111,7 +111,7 @@ function ttaNewTask (name) {
 function ttaAddTask (obj) {
     ttaProject.objTasks[obj.task_name] = obj;
     ttaProject.cntTasks += 1;
-    console.log(ttaProjects.cntTasks, obj.task_name);
+    console.log(ttaProject.cntTasks, obj.task_name);
 }
 
 function ttaTaskDuration (hours, minutes, seconds) {
