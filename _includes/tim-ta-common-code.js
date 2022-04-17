2556e95a2640dc92648ebde74683de5381c0bbef
pippim <92641463+pippim@users.noreply.github.com>
2022-04-16 20:24:05 -0600

    Give real durations

--- _includes/tim-ta-common-code.js
+++ _includes/tim-ta-common-code.js
@@ -76,13 +76,13 @@ Tasks Table (Small Screen)
   <col class="col8"/>
   <tr><th>Run</th><th>Up</th><th>Down</th><th>Edit</th><th>Delete</th><th>Controls</th>
   <th>Project Name</th><th># Tasks</th><th>Duration</th></tr>
-  <tr><td colspan="9"><table><tr><td>Why is this here?</td></tr></table></td></tr>
+  <!-- <tr><td colspan="9"><table><tr><td>Optional Heading</td></tr></table></td></tr> -->
   <tr><td>Run</td><td>Up</td><td>Down</td><td>Edit</td><td>Delete</td><td>Controls</td>
-  <td>Laundry</td><td># Tasks</td><td>Duration</td></tr>
+  <td>Laundry</td><td>3</td><td>1 hour, 40 minutes, 10 seconds</td></tr>
   <tr><td>Run</td><td>Up</td><td>Down</td><td>Edit</td><td>Delete</td><td>Controls</td>
-  <td>Chili</td><td># Tasks</td><td>Duration</td></tr>
+  <td>Chili</td><td>5</td><td>4 hours, 20 minutes</td></tr>
   <tr><td>Run</td><td>Up</td><td>Down</td><td>Edit</td><td>Delete</td><td>Controls</td>
-  <td>Work Out</td><td># Tasks</td><td>Duration</td></tr>
+  <td>Work Out</td><td>7</td><td>45 minutes</td></tr>
   <tr><td>Run</td><td>Up</td><td>Down</td><td>Edit</td><td>Delete</td><td>Controls</td>
   <td>Project Name</td><td># Tasks</td><td>Duration</td></tr>
 </table>
