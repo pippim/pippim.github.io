053317c24daf917b1cb2b83f31fa55afc5c96c5b
pippim <92641463+pippim@users.noreply.github.com>
2022-04-17 10:15:22 -0600

    Create default ttaStore (Tim-ta configuration)

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -7,193 +7,113 @@
 // dragElement defined in /assets/js/theCookieMachine.js
 // dragElement(document.getElementById("tta_window"));
 
-// Global to all Tim-ta Projects
-var tta_projects = {
-    arrNames: [],
-    confirm_delete_phrase: "delete"
+// Configuration & Container for all Tim-ta Projects
+// Default below for creation, overwritten when retrieved from localStorage
+var tta_store = {
+    arrProjects: [],
+    objProjects: {},
+    cntProjects: 0,
+    confirm_delete_phrase: "y"
+    timer_prompt: "true",
+    timer_end_alarm: "true",
+    timer_end_filename: "Alarm_03.mp3",
+    timer_end_notification: "false",
+    run_set_times: 1,
+    set_prompt: "false",
+    set_end_alarm: "false",
+    set_end_filename: "Alarm_05.mp3",
+    set_end_notification: "false",
+    all_sets_prompt: "false",
+    all_sets_end_alarm: "false",
+    all_sets_end_filename: "Alarm_12.mp3",
+    all_sets_end_notification: "false",
+    progress_bar_update_seconds: 1
 }
 
-// Global to SINGLE Tim-ta Project
+// SINGLE Tim-ta Project
+// When value is "default" it is inherited from Configuration 
 var tta_project = {
-    name: "",
-    default_ask_to_begin_timer: true,
-    ask_to_begin_set: true,
-    ask_to_begin_all_sets: true,
-    default_alarm_timer_end: true,
-    default_sound_filename: null,
-    alarm_set_end: true,
-    set_sound_filename: null,
-    alarm_all_sets_end: true,
-    all_sets_sound_filename: null,
-    default_notification_timer_end: true,
-    notification_set_end: true,
-    notification_all_sets_end: true,
-    progress_bar_update_seconds: 1,
-    window_position: null,
-    ask_save_window_position: true,
-    project_source: "Here"
+    project_index: null,
+    project_name: null,
+    objTasks: {},
+    cntTasks: 0,
+    timer_prompt: "default",
+    timer_end_alarm: "default",
+    timer_end_filename: "default",
+    timer_end_notification: "default",
+    run_set_times: "default",
+    set_prompt: "default",
+    set_end_alarm: "default",
+    set_end_filename: "default",
+    set_end_notification: "default",
+    all_sets_prompt: "default",
+    all_sets_end_alarm: "default",
+    all_sets_end_filename: "default",
+    all_sets_end_notification: "default",
+    progress_bar_update_seconds: "default"
 }
 
-// Global to SINGLE Timer within a Tim-ta Project
+// SINGLE Timer within a Tim-ta Project
+// When value is "default" it is inherited from Project 
 var tta_timer = {
-    name: "",
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    ask_to_begin_timer: true,
-    image_filename: null,
-    notification_timer_end: true,
-    alarm_timer_end: true,
-    timer_sound_filename: null,
+    timer_index: null,
+    timer_name: null,
+    hours: null,
+    minutes: null,
+    seconds: null,
+    timer_prompt: "default",
+    timer_end_alarm: "default",
+    timer_end_filename: "default",
+    timer_end_notification: "default",
+    progress_bar_update_seconds: "default"
 }
 
-document.querySelector('#tta_button').addEventListener('click', () => {
-  // TCM button click on webpage header
-  document.querySelector('#tta_window').style.cssText = `
-    display: flex;
-    flex-direction: column;
-  `;
-  // Make tta_button invisible
-  document.querySelector('#tta_button').style.cssText = `
-    opacity: 0.0;
-    background: transparent;
-    background-image: none;
-    border: none;
-  `;
-});
+// Get variable values and source.
+// EG timer_prompt value & source can be "true", "Manual Override"
+// "true", "Project Default", "true", "Configuration Default"
+var ttaStore, ttaProject, ttaTask;
 
-document.querySelector('#tta_window_close').addEventListener('click', () => {
-  // Hide tta_window
-  document.querySelector('#tta_window').style.display = "none";
-  // Make tta_button (main page header) visible?
-  if (vis_this_page == "true") { makeTcmButtonVisible() }
-});
+ttaNewConfig();  // Always new until localStorage setup
 
+function ttaNewConfig() {
+    ttaStore = tta_store;
+    ttaProject = tta_project;
+    ttaProject.project_name = "Laundry";
 
-const tta_body = document.getElementById('tta_window_body')  // Website tree entries html codes
+    ttaTask = ttaNewTask("Wash Cycle");
+    ttaTaskDuration(0, 16, 30);
+    ttaAddTask(ttaTask);
 
-document.querySelector('#tta_display_home').addEventListener('click', () => {
-    // arrConfigYml in search.js required by two TCM Window Buttons - Home & Webpage Info
-    var html = htmlFrontMatter(arrConfigYml, "Site Front Matter ('_config.yml')");
-    tta_body.innerHTML = html; // Update TCM Window body
-});
+    ttaTask = ttaNewTask("Rinse Cycle");
+    ttaTaskDuration(0, 13, 15);
+    ttaAddTask(ttaTask);
 
-document.querySelector('#tta_display_cloud').addEventListener('click', () => {
-    // Display Website Tree
-    fetch(raw_url + '/assets/json/website_tree.json')
-      .then((response) => response.json())
-      .then((website_tree) => {
-        var html = htmlWebsiteTree(website_tree);
-        tta_body.innerHTML = html; // Update TCM Window body
-      });
-});
+    ttaTask = ttaNewTask("Dryer");
+    ttaTaskDuration(0, 58, 0);
+    ttaAddTask(ttaTask);
 
-document.querySelector('#tta_display_local').addEventListener('click', () => {
-    // Display cookies and cache (WIP)
-    var html = htmlVisibilitySwitches();
-    html += htmlSearchStats();
-    html += htmlScreenInfo();
-    tta_body.innerHTML = html;
-
-    /*  Process TCM Window Button Visibility slider switches - shared  with ~/tcm.md
-        USE: % include tcm-common-code.js %} */
-    tcmButtonVisibility()});
-
-document.querySelector('#tta_hyperlink_recipe').addEventListener('click', () => {
-    processHyperlinkRecipe('tta_window_body');
-});
-
-document.querySelector('#tta_webpage_info').addEventListener('click', () => {
-    // Display webpage info - filename, front matter and text (WIP)
-    // raw_url set in search.js loaded before us
-    var urlMarkdown = getMarkdownFilename();
-
-    fetch(urlMarkdown)
-        .then((response) => response.text())
-        .then((results) => {
-            var results = results.split("\n")  // Convert string into array
-            var front_yml = getFrontMatter(results)
-            var html = htmlFrontMatter(front_yml, "Current Webpage Front Matter");
-            html += htmlWindowInfo();
-            html += htmlNavigatorInfo();
-            tta_body.innerHTML = html; // Update TCM Window body
-        });
-});
-
-document.querySelector('#tta_cookie_jar').addEventListener('click', () => {
-    // Display webpage info - filename, front matter and text (WIP)
-    // raw_url set in search.js loaded before us
-    // https://www.javascripttutorial.net/web-apis/javascript-notification/
-    var html = '<div class="container">\n' +
-               '<h1>JavaScript Notification API Demo</h1>\n' +
-               '<div class="error"></div>\n' +
-               '</div>'
-    var fileDownload="https://pippim.com/assets/img/TCM Header with Gingerbread Man.png"
-    //alert('About to download ' + fileDownload);
-    tta_body.innerHTML = html;
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attributes
-    //let downloading = downloads.download({url: fileDownload})
-    //console.log("downloading: " + downloading)
-
-    // Notifications
-    (async () => {
-        // create and show the notification
-        const showNotification = () => {
-            // create a new notification
-            const notification = new Notification('{{ site.title }} Timed Tasks', {
-                body: 'File has been downloaded.',
-                icon: '{{ site.url }}/favicon.png'
-            });
-
-            // close the notification after 10 seconds
-            setTimeout(() => {
-                notification.close();
-            }, 10 * 1000);
-
-            // navigate to a URL when clicked
-            notification.addEventListener('click', () => {
-
-                window.open('https://www.javascripttutorial.net/web-apis/javascript-notification/', '_blank');
-            });
-        }
-
-        // show an error message
-        const showError = () => {
-            const error = document.querySelector('.error');
-            error.style.display = 'block';
-            error.textContent = 'You blocked the notifications';
-        }
-
-        // check notification permission
-        let granted = false;
-
-        if (Notification.permission === 'granted') {
-            granted = true;
-        } else if (Notification.permission !== 'denied') {
-            let permission = await Notification.requestPermission();
-            granted = permission === 'granted' ? true : false;
-        }
-
-        // show notification or error
-        granted ? showNotification() : showError();
-
-    })();
-});
-
-/*
-function introduction_to_html() {
-    var html = "<p>";
-    html += "<h3>The Cookie Machine (TCM) Future Applications:</h3>\n";
-    html += "  ☑ View cookies used on the {{ site.title }} website.<br>\n";
-    html += "  ☑ Send cookie via mail. For backup or sharing.<br>\n";
-    html += "  ☑ Receive cookie via mail. From yourself or colleague.<br>\n";
-    html += "  ☑ Countdown Timers. For multi-phase time sensitive missions.<br>\n";
-    html += "  ☑ And in the future... Other ways of sharing/using Cookies.\n";
-    html += "</p>";
-    tta_body.innerHTML = html;              // Update TCM Window body
+    ttaStore.arrProjects = [ttaProject.project_name];
+    ttaStore.objProjects[ttaProject.project_name] = ttaProject;
+    ttaStore.cntProjects = 1;
 }
 
-introduction_to_html()  // Load immediately
-*/
+function ttaNewTask (name) {
+    var new_task = tta_task;
+    new_task.timer_index = ttaProject.cntTasks;
+    new_task.task_name = name;
+    new_task.hours = new_task.minutes = new_task.seconds = 0;
+    return new_task;
+}
+
+function ttaAddTask (obj) {
+    ttaProject.objTasks[obj.task_name] = obj;
+    ttaProject.cntTasks += 1;
+}
+
+function ttaTaskDuration (hours, minutes, seconds) {
+    ttaTask.hours = hours;
+    ttaTask.minutes = minutes;
+    ttaTask.seconds = seconds;
+}
 
 /* End of /assets/js/tim-ta.js */
