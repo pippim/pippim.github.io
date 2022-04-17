153fdec15e129dd2954e589bdadbd799f8c2a551
pippim <92641463+pippim@users.noreply.github.com>
2022-04-16 19:48:52 -0600

    show/hide columns testing

--- _includes/tim-ta-common-code.js
+++ _includes/tim-ta-common-code.js
@@ -64,5 +64,32 @@ Tasks Table (Small Screen)
 
 -->
 
+<table id='projects' border=1>
+  <col class="col1"/>
+  <col class="col2"/>
+  <col class="col3"/>
+  <col class="col4"/>
+  <tr><td colspan="4"><table><tr><td></td></tr></table></td></tr>
+  <tr><td>  2</td><td>   two</td><td>   deux</td><td>     zwei</td></tr>
+  <tr><td>  3</td><td> three</td><td>  trois</td><td>     drei</td></tr>
+  <tr><td>  4</td><td>  four</td><td>quattre</td><td>     vier</td></tr>
+  <tr><td>  5</td><td>  five</td><td>   cinq</td><td>fÜnf</td></tr>
+  <tr><td>  6</td><td>   six</td><td>    six</td><td>    sechs</td></tr>
+</table>
+<form>
+  Enter column no: <input type='text' name=col_no><br>
+  <input type='button' onClick='javascript:show_hide_column(col_no.value,  true);' value='show'>
+  <input type='button' onClick='javascript:show_hide_column(col_no.value, false);' value='hide'>
+</form>
+
+<script>
+function show_hide_column(col_no, do_show) {
+   var tbl = document.getElementById('projects');
+   var col = tbl.getElementsByTagName('col')[col_no];
+   if (col) {
+     col.style.visibility=do_show?"":"collapse";
+   }
+}
+</script>
 
 <!-- End of /_includes/tim-ta-common-code.js -->
