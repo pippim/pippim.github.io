0e497947c6886a6e8de76292662e0f3b7ad37675
pippim <92641463+pippim@users.noreply.github.com>
2022-04-18 19:01:39 -0600

    Use undefined instead of null for new values

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -81,10 +81,10 @@ var tta_project = {
 // SINGLE Timer within a Tim-ta Project
 // When value is "default" it is inherited from Project 
 var tta_task = {
-    task_name: null,
-    hours: null,
-    minutes: null,
-    seconds: null,
+    task_name: undefined,
+    hours: undefined,
+    minutes: undefined,
+    seconds: undefined,
     task_prompt: "default",
     task_end_alarm: "default",
     task_end_filename: "default",
