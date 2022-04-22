d38ef1325dcd267781e798a11a1f453fc88280e7
pippim <92641463+pippim@users.noreply.github.com>
2022-04-21 18:26:28 -0600

    One console.log no longer necessary

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -165,8 +165,8 @@ function updateRadioSounds () {
     for (const key of Object.keys(data_dictionary)) {
         if ( key.startsWith("fail_test") ) { continue; }
         get_dd_field(key);
-        console.log("key:", key, ".name:", dd_field.name, "type:", dd_field.type,
-                    "lower:", dd_field.lower);
+        //console.log("key:", key, ".name:", dd_field.name, "type:", dd_field.type,
+        //            "lower:", dd_field.lower);
         if (dd_field.type == "radio" && dd_field.lower == "sound_filenames") {
             console.log("+++ About to replace sound_filenames for field: " + key);
         }
