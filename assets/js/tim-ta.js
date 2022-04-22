627ea1bfe1c9aec2f4a4186acda1448df5ccfad9
pippim <92641463+pippim@users.noreply.github.com>
2022-04-21 18:10:31 -0600

    Display dd.field > .name .type and .lower

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -163,8 +163,11 @@ function get_dd_field (name) {
 function updateRadioSounds () {
     /* Called on load and after drag & drop sound files */
     for (const key of Object.keys(data_dictionary)) {
+        if key.startsWith("test_fail") { continue; }
+        get_dd_field(key);
+        console.log("key:", key, ".name:", dd_field.name, "type:", dd_file.type,
+                    "lower:", dd_field.lower)
         console.log("key:", key)
-        //get_dd_field(key);
         //if (dd_field.type == "radio" && dd_field.lower == "sound_filenames") {
         //    alert("About to replace sound_filenames for field: " + key)
         //}
