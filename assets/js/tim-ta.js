32a8c3ff181a96d369d39b86eff8548c056dff08
pippim <92641463+pippim@users.noreply.github.com>
2022-04-24 11:53:58 -0600

    console.log("ttaTask:", ttaTask);

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -596,6 +596,7 @@ function clickListen(i) {
     /* prefix "f" short for "flag" where variable can contain "true" or "false" */
     fTaskEndAlarm = getTaskValue("task_end_alarm");
     fTaskEndNotify = getTaskValue("task_end_notification");
+    console.log("ttaTask:", ttaTask);
     var fSetEndAlarm, fSetEndNotify, fAllSetsEndAlarm, fAllSetsEndNotify;
     fSetEndAlarm = fSetEndNotify = fAllSetsEndAlarm = fAllSetsEndNotify = "false" ;    
 
