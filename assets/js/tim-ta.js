6ebee7d8ab58b2f929ad20242b6ec8fabcae6dca
pippim <92641463+pippim@users.noreply.github.com>
2022-04-24 10:14:42 -0600

    JSON.stringify(inpSelects[nane]));

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1219,13 +1219,13 @@ function getInputValues() {
     // Get switch values and add to formValues
     for (const name of Object.keys(inpSwitches)) {
         if (name == null) { alert ("inpSwitches undefined!"); continue; }
-        console.log("inpSwitches[nane]:", name, inpSwitches[nane]);
+        console.log("inpSwitches[nane]:", JSON.stringify(inpSwitches[nane]));
         formValues[name] = inpSwitches[name].value;
     }
     // Add select values to formValues
     for (const name of Object.keys(inpSelects)) {
         if (name == null) { alert ("inpSelects undefined!"); continue; }
-        console.log("inpSelects[nane]:", name, inpSelects[nane]);
+        console.log("inpSwitches[nane]:", JSON.stringify(inpSelects[nane]));
         formValues[name] = inpSelects[name].value;
     }
 
