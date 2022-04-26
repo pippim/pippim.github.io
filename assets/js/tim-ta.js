51b3b72d24ede475ab12c81450fc92f01341ecce
pippim <92641463+pippim@users.noreply.github.com>
2022-04-26 04:56:07 -0600

    Remove duplicate var html

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1105,8 +1105,9 @@ function buildInit() {
 }
 
 function buildLine() {
-    var html = '<tr style="border-bottom:1px solid black">\n';
-    var html += '<td> colspan="100%"></td></tr>\n";
+    var html = "";
+    html += '<tr style="border-bottom:1px solid black">\n';
+    html += '<td> colspan="100%"></td></tr>\n";
     return html;
 }
 
