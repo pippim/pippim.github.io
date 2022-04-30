cc98e45249ef0f201124065fd25d82a6ebc15c36
pippim <92641463+pippim@users.noreply.github.com>
2022-04-30 11:22:41 -0600

    let rect = p['elmLink'].getBoundingClientRect();

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -2087,10 +2087,17 @@ function popCreate(msg_type, msg, error_id, id_elm_type, id_elm) {
         p['elmLink'] = ttaElm;
     }
 
+
     var elmDraggable = document.getElementById(p['idWindow']);  // ID inside <div>
+    // .getBoundingClientRect()
+    // .getClientRect
+    // window.scrollY
+    let rect = p['elmLink'].getBoundingClientRect();
+    var pos3 = rect.left;  // Get link (anchor reference point)
+    var pos4 = rect.top;  //  x (left) and y (top
     var elmHeader = document.getElementById(p['idWindow'] + "_header");
-    var pos3 = p['elmLink'].style.left;  // Get link (anchor reference point)
-    var pos4 = p['elmLink'].style.top;  //  x (left) and y (top
+    //var pos3 = p['elmLink'].style.left;  // Get link (anchor reference point)
+    //var pos4 = p['elmLink'].style.top;  //  x (left) and y (top
     // BELOW two errors: Error in parsing value for “top”.  Declaration dropped.
     console.log("pos3 =", pos3, "px");
     console.log("pos4 =", pos4, "px");
