dc9c68ae01ae35a45053fa303afaebd4286d8559
pippim <92641463+pippim@users.noreply.github.com>
2022-04-30 11:00:03 -0600

    var pos3 = p['elmLink'].style.left

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -2088,8 +2088,8 @@ function popCreate(msg_type, msg, error_id, id_elm_type, id_elm) {
 
     var elmDraggable = document.getElementById(p['idWindow']);  // ID inside <div>
     var elmHeader = document.getElementById(p['idWindow'] + "_header");
-    var pos3 = p['elmLink'].clientX;  // Get link (anchor reference point)
-    var pos4 = p['elmLink'].clientY;  //  x (left) and y (top
+    var pos3 = p['elmLink'].style.left;  // Get link (anchor reference point)
+    var pos4 = p['elmLink'].style.top;  //  x (left) and y (top
     // BELOW two errors: Error in parsing value for “top”.  Declaration dropped.
     console.log("pos3 =", pos3, "px");
     console.log("pos4 =", pos4, "px");
