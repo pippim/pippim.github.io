91b298987f4744d4f244d2c576e7689df41fe395
pippim <92641463+pippim@users.noreply.github.com>
2022-04-30 16:25:42 -0600

    Mount error message relative to progress bar

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1041,14 +1041,16 @@ function progressTouched(i, element) {
     }
 
     popClearByError("total_task_clicked");
-    if (activeBarNo == 0) {
-        popCreate("e", "Cannot select a total progress bar", "total_task_clicked");
+    if (boolTotalBar) {
+        popCreate("e", "Cannot select a total progress bar", "total_task_clicked",
+                  "elm", element);
         return;
     }
 
     popClearByError("task_not_running");
-    if (activeBarNo == 0) {
-        popCreate("e", "Can only select a running progress bar", "task_not_running");
+    if (!boolTouchedActive == 0) {
+        popCreate("e", "Can only select a running progress bar", "task_not_running"
+                  "elm", element);
         return;
     }
 
