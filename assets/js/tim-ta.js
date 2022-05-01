947d0529abb94ddc1f7090f8d81613b191743fbc
pippim <92641463+pippim@users.noreply.github.com>
2022-05-01 09:48:26 -0600

    Double Left & Right Arrows for FR, FF

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1093,18 +1093,24 @@ function buildProgressControlButtons(i) {
         - 23ED ⏭︎ skip to end, next
         }
     &play.fast.forward; - fast forward. fast-forward button for skipping along a movie or sound, as on cassette or CD player (cf. play.stop, play.pause, play.start, play.fast.reverse).
+        "forward", "&play.fast.forward;", "Fast forward", "pcbClickForward(" + i +")",
         "forward", "&#x23E9;", "Fast forward", "pcbClickForward(" + i +")",
+        DOUBLE RIGHT ARROW:
+        "forward", "&#xBB;", "Fast forward", "pcbClickForward(" + i +")",
 [test: &play.fast.reverse;]
+        "rewind", "&play.fast.reverse;", "Rewind, Fast backwards", "pcbClickRewind(" + i +")",
         "rewind", "&#x23EA;", "Rewind, Fast backwards", "pcbClickRewind(" + i +")",
+        DOUBLE LEFT ARROW:
+        "rewind", "&#xAB;", "Rewind, Fast backwards", "pcbClickRewind(" + i +")",
 
     */
 
     workTask = Object.assign({}, ttaTask); // https://stackoverflow.com/a/34294740/6929343
     var arrButtons = [
         "begin", "&#x23EE;", "Skip to start, Previous", "pcbClickBegin(" + i +")",
-        "rewind", "&play.fast.reverse;", "Rewind, Fast backwards", "pcbClickRewind(" + i +")",
+        "rewind", "&#xAB;", "Rewind, Fast backwards", "pcbClickRewind(" + i +")",
         "play_toggle", "&#x23EF;︎", "Play/Pause toggle", "pcbClickPlayPause(" + i +")",
-        "forward", "&play.fast.forward;", "Fast forward", "pcbClickForward(" + i +")",
+        "forward", "&#xBB;", "Fast forward", "pcbClickForward(" + i +")",
         "end", "&#x23ED;", "Skip to end, Next", "pcbClickEnd(" + i +")"
     ]
 
