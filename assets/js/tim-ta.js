b67972469547f93da73590695a525acea9951ded
pippim <92641463+pippim@users.noreply.github.com>
2022-05-02 18:06:49 -0600

    min-width: 400px;

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -2547,7 +2547,7 @@ function popBuildStyle(msg_type) {
     html += '.msgq-window-buttons {\n' +
             '  display: flex;\n' +
             '  justify-content: space-around;\n' +
-            '  min-width: 25rem;\n' +
+            '  min-width: 400px;\n' +
             //'  margin: .25rem;\n' +
             '  padding: .25rem;\n' +
             '  border: 2px solid grey;\n' +
