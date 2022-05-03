d485dd9179f2c3001fcf9264c3fa284e2fb00327
pippim <92641463+pippim@users.noreply.github.com>
2022-05-03 04:55:08 -0600

    listen for touch before mouse

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -2604,18 +2604,18 @@ function dragElement(elm) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elm.id + "_header")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elm.id + "_header").onmousedown = dragMouseDown;
     document.getElementById(elm.id + "_header").touchstart = dragTouchStart;
+    document.getElementById(elm.id + "_header").onmousedown = dragMouseDown;
     // https://stackoverflow.com/a/52554777/6929343
 
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
-    elm.onmousedown = dragMouseDown;
     elm.touchstart = dragTouchStart;
+    elm.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
-    e = e || window.event;
+    e = e || window.event;  // if the parameter e not passed, default to after the ||"
     e.preventDefault();  // Prevents text highlighting while dragging header
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
