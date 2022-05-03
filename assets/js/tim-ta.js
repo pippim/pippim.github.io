09fef58a0f2d844f35b3dc4b7fdbed4b37034942
pippim <92641463+pippim@users.noreply.github.com>
2022-05-02 18:44:45 -0600

    background-color: header_bg_color;

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -544,6 +544,9 @@ function bigFootStyle() {
             '  padding: .25rem .5rem;\n' +
             '  border: 3px solid;\n' +
             '  border-radius: 1rem;\n' +
+            '  background-color: header_bg_color;\n' +
+            '  background-image: linear-gradient(120deg, \n' +
+            '    header_bg_color_secondary, header_bg_color);\n' +
             '}\n' +
             '.leftFoot, .centerFoot, .rightFoot {\n' +
             //'  flex-grow: 1;\n' +
@@ -2558,6 +2561,9 @@ function popBuildStyle(msg_type) {
             '  padding: .25rem;\n' +
             '  border: 2px solid grey;\n' +
             '  border-radius: 1rem;\n' +
+            '  background-color: header_bg_color;\n' +
+            '  background-image: linear-gradient(120deg, \n' +
+            '    header_bg_color_secondary, header_bg_color);\n' +
             '}\n';
 
     html += '.msgq-window-button {\n' +
