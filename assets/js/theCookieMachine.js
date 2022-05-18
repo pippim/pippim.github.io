d695babde58d4b5b9cdf5c115336a2360961ce35
pippim <92641463+pippim@users.noreply.github.com>
2022-05-18 17:21:44 -0600

    if (tcmButtonClasses.length == 0) { break }

--- assets/js/theCookieMachine.js
+++ assets/js/theCookieMachine.js
@@ -94,7 +94,7 @@ if (tcmButtonId !== null) {
 
 for (var ndx = 0; ndx < tcmButtonClasses.length; ndx++) {
     // Listen for TCM button click on webpage header by tcm-button class
-    if (tcmButtonClasses.length == 0) { return }
+    if (tcmButtonClasses.length == 0) { break }
     tcmButtonClasses[ndx].addEventListener('click', () => {
         // Display the TCM draggable window
         document.querySelector('#tcm_window').style.cssText = `
