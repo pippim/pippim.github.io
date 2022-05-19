11b510be901a560f0c9a2f8986ffa160ca1d5f90
pippim <92641463+pippim@users.noreply.github.com>
2022-05-18 20:13:56 -0600

    Redeclare array/object tcmButtonClasses

--- assets/js/theCookieMachine.js
+++ assets/js/theCookieMachine.js
@@ -89,8 +89,8 @@ if (tcmButtonId !== null) {
     });
 }
 
-// Copy from tcm-common-code.js because it's undefined for some reason??
-// var tcmButtonClasses = document.getElementsByClassName("tcm-button");  // New class
+// Copy from tcm-common-code.js because it's undefined for some reason?? DOM not loaded?
+tcmButtonClasses = document.getElementsByClassName("tcm-button");  // New class
 
 for (var ndx = 0; ndx < tcmButtonClasses.length; ndx++) {
     // Listen for TCM button click on webpage header by tcm-button class
