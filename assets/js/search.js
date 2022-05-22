5c2c420ae8307cf81f9dc8cc7fe5efbbf35c7eaa
pippim <92641463+pippim@users.noreply.github.com>
2022-05-22 17:38:54 -0600

    elm.style.backgroundColor = "#787878"

--- assets/js/search.js
+++ assets/js/search.js
@@ -345,19 +345,19 @@ function reverseContentDimmed() {
     //document.getElementById('content').style.backgroundColor = saveBackgroundColor
     var elm = document.getElementById('content')
     //elm.style.filter = "brightness(1)"
-    elm.style.filter = "blur(0)"
-    //elm.style.backgroundColor = "white"
-    elm.style.filter = "invert(0)"
-    elm.style.opacity = 1
+    //elm.style.filter = "blur(0)"
+    elm.style.backgroundColor = "white"
+    //elm.style.filter = "invert(0)"
+    //elm.style.opacity = 1
     if (elmBody !== null) { elmBody.style.overflow = "auto" }
 }
 function setContentDimmed() {
     var elm = document.getElementById('content')
     //elm.style.filter = "brightness(.15)"
-    elm.style.filter = "blur(.1)"
-    //elm.style.backgroundColor = "silver"
-    elm.style.filter = "invert(.5)"
-    elm.style.opacity = .8
+    //elm.style.filter = "blur(.1)"
+    elm.style.backgroundColor = "#787878"
+    //elm.style.filter = "invert(.5)"
+    //elm.style.opacity = .8
     if (elmBody !== null) { elmBody.style.overflow = "hidden" }
 
     var style = elm.currentStyle || window.getComputedStyle(elm);
