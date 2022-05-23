0e22f9f968d256bea031bbdacaf0d8f22ca79bd7
pippim <92641463+pippim@users.noreply.github.com>
2022-05-23 11:17:25 -0600

    May 23, 2022 Author update (add comments)

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,6 +1,7 @@
 // Copy code block contents to clipboard. 
 // See _includes/copyHeader.html for credit
 
+// This assumes that you're using Rouge; if not, update the selector
 const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
@@ -8,10 +9,22 @@ copyCodeButtons.forEach((copyCodeButton, index) => {
   const code = codeBlocks[index].innerText;
 
   copyCodeButton.addEventListener('click', () => {
+    // Copy the code to the user's clipboard
     window.navigator.clipboard.writeText(code);
+
+    // Update the button text visually
+    /* NEW CODE published May 23, 2022:
+        const { innerText: originalText } = copyCodeButton;
+        copyCodeButton.innerText = 'Copied!';
+    */
+    // (Optional) Toggle a class for styling the button
     copyCodeButton.classList.add('copied');
 
+    // After 2 seconds, reset the button to its initial UI
     setTimeout(() => {
+      /* NEW CODE published May 23, 2022:
+          copyCodeButton.innerText = originalText;
+      */
       copyCodeButton.classList.remove('copied');
     }, 2000);
   });
