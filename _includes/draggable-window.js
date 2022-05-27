17299fdb258d56fd6615592e9ca21f046b544d38
pippim <92641463+pippim@users.noreply.github.com>
2022-05-26 19:59:36 -0600

    Restore file deleted in error

---
+++ _includes/draggable-window.js
@@ -0,0 +1,74 @@
+/*  /_includes/draggable-window.js
+
+    Drag window by title bar with mouse on desktop or with finger on mobile.
+
+*/
+
+// dragElement() copied from theCookieMachine.js
+function dragElement(elm) {
+    var offX = 0, offY = 0, oldX = 0, oldY = 0, x = 0, y = 0, useTouch = false;
+    if (document.getElementById(elm.id + "_header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elm.id + "_header").ontouchstart = dragTouchStart;
+        document.getElementById(elm.id + "_header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elm.ontouchstart = dragTouchStart;
+        elm.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        // get the mouse cursor position at startup
+        e.preventDefault();  // Prevents text highlighting while dragging header
+        setXY(e);
+        saveXY();
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function dragTouchStart(e) {
+        // get the touch position at startup
+        e.preventDefault();  // Prevents text highlighting while dragging header
+        useTouch = true;
+        setXY(e);
+        saveXY();
+        document.ontouchend = closeDragElement;
+        document.ontouchmove = elementDrag;
+    }
+
+    function setXY(e) {
+        if(useTouch) {
+            x = parseInt(e.targetTouches[0].clientX);  // bunch of decimals
+            y = parseInt(e.targetTouches[0].clientY);
+        } else {
+            x = e.clientX;  // already integer for mouse down
+            y = e.clientY;
+        }
+    }
+
+    function saveXY() {
+        oldX = x;
+        oldY = y;
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();  // Prevents text highlighting while dragging header
+        // calculate the new cursor position:
+        setXY(e);
+        offX = oldX - x;
+        offY = oldY - y;
+        saveXY();
+        elm.style.left = (elm.offsetLeft - offX) + "px";
+        elm.style.top = (elm.offsetTop - offY) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released or touch ends:
+        document.onmouseup = null;
+        document.onmousemove = null;
+        document.ontouchend = null;
+        document.ontouchmove = null;
+    }
+}  // End of dragElement2(elm)
+
+/* End of /_includes/draggable-window.js */
