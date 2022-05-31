6f207fe4527c0f33d671d3f0bb2a47745c36b53c
pippim <92641463+pippim@users.noreply.github.com>
2022-05-31 04:46:04 -0600

    ttaTask = Object.assign({}, tta_task)

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -2362,11 +2362,19 @@ function configClickUpload() {
         for (var j=0; j<arrProjects.length; j++) {
             var objProject = objProjects[arrProjects[j]]
             var existingProject = (ttaConfig.arrProjects.includes(objProject['project_name']))
+            if (existingProject == true) ttaProject = ttaConfig.objProjects[objProject['project_name']]
+                                    else ttaProject = Object.assign({}, tta_project)
             console.log(objProject['project_name'], "is an existing project?", existingProject)
             var arrTasks = objProject['arrTasks']
             var objTasks = objProject['objTasks']
             for (var k=0; k<arrTasks.length; k++) {
                 var objTask = objTasks[arrTasks[k]]
+                var existingTask = (existingProject == true &&
+                                    .arrProjects.includes(objProject['project_name']))
+                if (existingTask == true) ttaTask = ttaProject.objTasks[objTask['task_name']]
+                                     else ttaTask = Object.assign({}, tta_task)
+                console.log(objTask['task_name'], "is an existing task?", existingTask)
+
                 //console.log("Project:", objProject['project_name'],
                 //            "Task:", objTask['task_name'])
             }
