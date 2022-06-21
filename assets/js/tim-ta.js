ef3328d766d9fb3d47d438325100296d321869ba
pippim <92641463+pippim@users.noreply.github.com>
2022-06-21 04:29:47 -0600

    var sec = +ttaTask.seconds + 0
    Simply type cohersion

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -949,23 +949,24 @@ function tabRunTimersDetail(i) {
 
     secondsTask = 0;  // Abundant caution.
     //var sec = +ttaTask.seconds;  // Only works in Chrome and Firefox
-    var sec = +ttaTask.seconds * 1;  // Add support for MS Edge
-    sec += +ttaTask.minutes * 60;  // Tricky you can multiply a +String
-    sec += +ttaTask.hours * 60 * 60;
-    secondsTask = parseInt(sec, 10);
-    secondsSet += secondsTask;
+    var sec = +ttaTask.seconds + 0  // Add support for MS Edge
+    sec += +ttaTask.minutes * 60  // Tricky you can multiply a +String
+    sec += +ttaTask.hours * 60 * 60
+    secondsTask = sec
+    secondsSet += secondsTask
     //console.log("ttaProject:", ttaProject);  // What does run_set_times contain?
-    run_set_times = parseInt(getProjectValue('run_set_times'), 10);
-    secondsAllSets += secondsTask * run_set_times;
+    run_set_times = Number(getProjectValue('run_set_times'))
+    // run_set_times = Number(getProjectValue('run_set_times')
+    secondsAllSets += secondsTask * run_set_times
+
     // Below Edge Invalid Time error but not Chrome or FireFox parseInt
     //hhmmssTask = new Date(secondsTask * 1000).toISOString().substr(11, 8);
-//    if (!(debugTime)) {
-        var milliTask = secondsTask * 1000
-        var dateTask = new Date(milliTask)
-        console.log("milliTask:", milliTask, typeof milliTask, "dateTask:", dateTask)
-        console.log("dateTask.toISOString():", dateTask.toISOString().substr(11, 8))
-//    }
+    var milliTask = secondsTask * 1000
+    var dateTask = new Date(milliTask)
+    console.log("milliTask:", milliTask, typeof milliTask, "dateTask:", dateTask)
+    console.log("dateTask.toISOString():", dateTask.toISOString().substr(11, 8))
     //hhmmssTask = new Date(+secondsTask * 1000).toISOString().substr(11, 8);
+
     hhmmssTask = new Date(parseInt(secondsTask * 1000, 10)).toISOString().substr(11, 8);
     hhmmssSet = new Date(+secondsSet * 1000).toISOString().substr(11, 8);
     hhmmssAllSets = new Date(+secondsAllSets * 1000).toISOString().substr(11, 8);
