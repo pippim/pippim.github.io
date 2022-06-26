b3a82adedff629266d8b506b7f98ad6a751d764d
pippim <92641463+pippim@users.noreply.github.com>
2022-06-26 08:05:43 -0600

    Add console.log to track not a number

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -965,7 +965,10 @@ function tabRunTimersDetail(i) {
     var milliTask = convertNumber(secondsTask) * 1000
     var dateTask = new Date(milliTask)
     console.log("milliTask:", milliTask, typeof milliTask, "dateTask:", dateTask)
-    //console.log("dateTask.toISOString():", dateTask.toISOString().substr(11, 8))
+    console.log("secondsTask:", secondsTask, "secondsSet:", secondsSet,
+                "secondsAllSets:", secondsAllSets,
+                "dateTask.toISOString():", dateTask.toISOString().substr(11, 8))
+
     //hhmmssTask = new Date(+secondsTask * 1000).toISOString().substr(11, 8);
 
     hhmmssTask = new Date(secondsTask * 1000).toISOString().substr(11, 8);
