6294e9a3b63b6613da2f62d5f5f3935ea71d1657
pippim <92641463+pippim@users.noreply.github.com>
2022-08-21 12:45:03 -0600

    console.log("popup window runWindow.id"

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -929,8 +929,8 @@ function paintRunTimers(i) {
     ttaRunElm.scrollIntoView();  // Scroll top level element into view
     scrSetSize();  // Call on document load. Must also call when RunTimers is painted
 
-    var newElm = window.open('','runTtaElm','directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=400,height=350');
-    console.log("popup window newElm.id", newElm.id)
+    var runWindow = window.open('','runWindowElm','directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=400,height=350');
+    console.log("popup window runWindow.id", runWindow.id)
     runAllTimers();  // Run through all timers
     // Get to this point instantly while runAllTimers() runs asynchronously
 }  // End of paintRunTimers(i)
