aa3379503bf45b4684246ffd78f1e8a2ae661677
pippim <92641463+pippim@users.noreply.github.com>
2022-08-28 14:09:36 -0600

    {{ site.url }}/assets/img/tim-ta/alarm-clock.jpg

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1255,10 +1255,9 @@ async function runAllTimers() {
             if (audioControl != null) {
                 // When !== null used, "TypeError: audioControl is undefined"
                 if (audioControl.ended) {}  // TODO: short audio ended already?
-                var popId = popCreate("a", "/assets/img/tim-ta/alarm-clock.jpg",
-                                      audioControl, "elm", ttaRunElm)
-                                      // TODO: Image is blank in Popup Window
-                                      // n popCreate( n popPrompt(
+                var popId = popCreate("a",
+                    "{{ site.url }}/assets/img/tim-ta/alarm-clock.jpg",
+                    audioControl, "elm", ttaRunElm)  // n popCreate( n popPrompt(
                 var elm = msgq[popId].elmWindow
                 while(true) {
                     await sleep(50);
@@ -3054,54 +3053,41 @@ function popBuildHtml(msg_type, msg, index, buttons) {
     if (msg_type == "i") { msg_head = "Info"; msgq_class = "msgq-info"; }
     if (msg_type == "s") { msg_head = "Success"; msgq_class = "msgq-success"; }
     if (msg_type == "a") { msg_head = "Alarm"; msgq_class = "msgq-success"; }
-    var html = "";
+    // Are we are running in main webpage, else popup
     var win = getWin()
+    if (win == window) var pre = ""
+                  else var pre = "window.opener."
+
+    var html = "";
     // For historical reasons must be "_header" not "-header" to drag window
     html += '<div id="popIndex' + index + '" class="msgq-window">\n';
     html += '  <div id="popIndex' + index + '_header" \n';
     html += '       class="msgq-window-header ' + msgq_class + '">' + msg_head +
                     '&emsp; (Click here to drag)\n';
     html += '    <span class="msgq-window-close closeBtn" \n';
-//    html += '      onclick="popClose(\'popIndex' + index + '\')" \n';
-    // Are we are running in main webpage, else popup
-    if (win == window) var pre = ""
-                  else var pre = "window.opener."
-
     html += '      onclick="' + pre + 'popClose(\'popIndex' + index + '\')" \n';
-    /*
-    if (window.opener == null)
-        html += '      onclick="popClose(\'popIndex' + index + '\')" \n'
-    else
-        html += '      onclick="window.opener.popClose(\'popIndex' + index + '\')" \n'
-    */
     html += '      >&#65336;\n';  // #65336 latin full x is latter: ✕XＸ
     html += '    </span>\n';
     html += '  </div>\n';
     html += '  <div class="msq-window-body">\n';
+    // alarm then msg="{{ site.url }}/assets/img/tim-ta/alarm-clock.jpg"
     if (msg_type == "a") html += '    <img src="' + msg + '" ' +
                                  'class="shake-image" ' +
                                  'width="200" height="200" ' +
                                  '/>\n'
-                    else html += '    <p>' + msg + '</p>\n'
+                    else html += '    <p>' + msg + '</p>\n'  // regular message
     html += '  </div>\n';
     html += '  <div class="msgq-window-buttons"> <!-- Buttons: OK -->\n';
     if (buttons == null) {
         html += '    <button class="tta-btn msgq-window-button"\n'
         html += '      title="Click to close" \n'
-        // html += '      onclick="popClose(\'popIndex' + index + '\')" \n'
         html += '      onclick="' + pre + 'popClose(\'popIndex' + index + '\')" \n'
-        /*
-        if (window.opener == null)
-            html += '      onclick="popClose(\'popIndex' + index + '\')" \n'
-        else
-            html += '      onclick="window.opener.popClose(\'popIndex' + index + '\')" \n'
-        */
         html += '       >OK</button>\n'
     } else html += htmlButtons(buttons)
     html += '  </div>\n';
     html += '</div>\n';
     return html;
-}  // Ebd if popBuildHtml(msg_type, msg, index, buttons)
+}  // End of popBuildHtml(msg_type, msg, index, buttons)
 
 function htmlButtons(buttons) {
     /* DECLARED ABOVE:
