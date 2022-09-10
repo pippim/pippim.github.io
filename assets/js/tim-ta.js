1f8ca9c3cfc5127a1f628e5a74e4e573c71ad35b
pippim <92641463+pippim@users.noreply.github.com>
2022-09-09 19:47:16 -0600

    Remove win.document and add console.log to trace

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -28,7 +28,11 @@ function scrSetSize() {
     else scrMedium = true
 
     var win = getWin()  // Will be main webpage or popup window
-    const x = win.document.getElementById("content")  /* Exists in every _layout */
+    console.log("scrWidth:", scrWidth, "(win == window)", (win == window))
+    //const x = win.document.getElementById("content")  /* Exists in every _layout */
+    // Scroll anchoring was disabled in a scroll container because of too many consecutive adjustments (10) with too little total distance (-2.44833335876465 px average, -24.4833 px total). tim-ta.html
+    const x = document.getElementById("content")  /* Exists in every _layout */
+    // When called from popup: Uncaught TypeError: x is null
     const y = x.getElementsByTagName("progress")  /* To override styling of progress { */
     for (var i=0; i<y.length; i++) {
         console.log("y[i].id:", y[i].id,
