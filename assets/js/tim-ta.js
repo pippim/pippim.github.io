ce4255feb3b194ae487401814705640b6956e719
pippim <92641463+pippim@users.noreply.github.com>
2022-09-11 15:01:27 -0600

    Debug restart timer

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1323,7 +1323,7 @@ function pcbClickBegin(i) {
     var entry = pcbClickCommon(i, "begin")
     entry.progress = 0
     entry.remaining = entry.seconds
-    secondsTaskPaused = 0
+    secondsTaskPaused = entry.seconds  // Test to fix?
     console.log("entry:", entry)
 }
 function pcbClickRewind(i) {
