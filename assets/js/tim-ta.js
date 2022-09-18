1b9c1d4d24f909fdae88b6e1668b2e2f337a3c6a
pippim <92641463+pippim@users.noreply.github.com>
2022-09-18 16:50:54 -0600

    function notifyMe(testMessage) {

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -1926,29 +1926,31 @@ function flashGrey(id) {
     }, 2000);
 }
 
-function notifyMe() {
-  if (!("Notification" in window)) {
-    // Check if the browser supports notifications
-    console.log("This browser does not support desktop notification")
-  } else if (Notification.permission === "granted") {
-    // Check whether notification permissions have already been granted;
-    // if so, create a notification
-    // const notification = new Notification("Notifications permitted.");
-    console.log("Notifications permitted.")
-    // …
-  } else if (Notification.permission !== "denied") {
-    // We need to ask the user for permission
-    Notification.requestPermission().then((permission) => {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        const notification = new Notification("Notifications permitted.");
-        // …
-      }
-    });
-  }
+function notifyMe(testMessage) {
+    if (!("Notification" in window)) {
+        // Check if the browser supports notifications
+        console.log("This browser does not support desktop notification")
+        return false
+    } else if (Notification.permission === "granted") {
+        // Check whether notification permissions have already been granted;
+        // if so, create a notification
+        // const notification = new Notification("Notifications permitted.");
+        console.log("Notifications permitted for:", testMessage)
+        return true
+    } else if (Notification.permission !== "denied") {
+        // We need to ask the user for permission
+        Notification.requestPermission().then((permission) => {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                const notification = new Notification(testMessage)
+                return true
+            }
+        });
+    }
 
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them anymore.
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+    return false
 }
 
 function paintConfigForm() {
@@ -2570,11 +2572,13 @@ function validateNotification(value, output) {
     if (value == "false") return true  // Not turned on
 
     console.log("Check Desktop Notification Permission for:", dd_field.name)
-    if (false == true) {
+    const permissionGranted = notifyMe("Notification test for:<br>" +
+                                       dd_field.label)
+    if (!permissionGranted) {
         popCreateUniqueError("e", dd_field.label + " requires browser permission.",
                              "notification_permission",
-                             "id", dd_field.name, null, output);
-        return false;
+                             "id", dd_field.name, null, output)
+        return false
     }
     popClearByError("notification_permission")
     return true
