212d4d02f1f08bd3da7754bd4155bb7c52f07571
pippim <92641463+pippim@users.noreply.github.com>
2022-09-18 12:18:13 -0600

    Check desktop notification permissions

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -6,6 +6,31 @@
 
 // console.log("browser popup:", browser.browserSettings.allowPopupsForUserEvents.get())
 
+function notifyMe() {
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    console.log("This browser does not support desktop notification")
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    // const notification = new Notification("Notifications permitted.");
+    console.log("Notifications permitted.")
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification("Notifications permitted.");
+        // …
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+}
+
 var scrTimeout, scrWidth, scrSmall, scrMedium, scrLarge
 
 function scrSetSize() {
