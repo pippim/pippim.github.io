dcb0e0ccd01dd4b8e4b21e304ec647106f0094b6
pippim <92641463+pippim@users.noreply.github.com>
2022-09-22 18:28:54 -0600

    Create getBrowser() from get_browser()

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -551,26 +551,27 @@ function convertVersion11() {
 
 convertVersion11()  // One time conversion
 
-function get_browser() {
-    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
-        return {name:'IE',version:(tem[1]||'')};
-        }
-    if(M[1]==='Chrome'){
+function getBrowser() {
+    var ua = navigator.userAgent, tem
+    var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []
+    if (/trident/i.test(M[1])) {
+        tem = /\brv[ :]+(\d+)/g.exec(ua) || []
+        return {name: 'IE', version: (tem[1] || '') }
+    }
+    if (M[1]==='Chrome') {
         tem=ua.match(/\bOPR|Edge\/(\d+)/)
-        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-        }
-    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+        if (tem!=null) return { name:'Opera', version:tem[1] }
+    }
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?']
+    if ((tem=ua.match(/version\/(\d+)/i))!=null) M.splice(1,1,tem[1])
     return {
-      name: M[0],
-      version: M[1]
-    };
+        name: M[0],
+        version: M[1]
+    }
  }
 
-var browser=get_browser(); // browser.name = 'Chrome'
-                           // browser.version = '40'
+var browser = getBrowser()  // browser.name = 'Chrome'
+                            // browser.version = '40'
 
 console.log("browser() and navigator")
 console.log(browser.name, browser.version)
