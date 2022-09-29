2bc70ed78beafb4b58ec3c4e2eef5452bb079f30
pippim <92641463+pippim@users.noreply.github.com>
2022-09-29 17:58:50 -0600

    display all :root color codes

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -158,6 +158,33 @@ function myFunction_set() {
     rootElm.style.setProperty(targetProperty, red);
 }
 
+console.log(Array.from(document.styleSheets)
+  .filter(
+    sheet =>
+      sheet.href === null || sheet.href.startsWith(window.location.origin)
+  )
+  .reduce(
+    (acc, sheet) =>
+      (acc = [
+        ...acc,
+        ...Array.from(sheet.cssRules).reduce(
+          (def, rule) =>
+            (def =
+              rule.selectorText === ":root"
+                ? [
+                    ...def,
+                    ...Array.from(rule.style).filter(name =>
+                      name.startsWith("--")
+                    )
+                  ]
+                : def),
+          []
+        )
+      ]),
+    []
+  );
+)
+
 var ttaStyleSheet, styles
 // 'styles' is shared with ttaRunStyleSheet
 
