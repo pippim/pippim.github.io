5a7dc4ae1de1b91b39f6eb7b8791f7137cbe69df
pippim <92641463+pippim@users.noreply.github.com>
2022-09-30 17:50:49 -0600

    darken: --nav-button-bg-color": "#302030"

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -41,7 +41,7 @@ var colorSchemeCayman = {
 }
 
 var colorSchemeDark = {
-    "--nav-button-bg-color": "#403040",
+    "--nav-button-bg-color": "#302030",
     "--msgq-error-bg-color": "#f44336",
     "--msgq-warning-bg-color": "#ff9800",
     "--msgq-info-bg-color": "#2196F3",
