e95f711b1628ca563722f0f86edeb9c6181a3bd2
pippim <92641463+pippim@users.noreply.github.com>
2022-09-30 16:12:01 -0600

    Test tcm-common-code.js visibility objTcmVisById

--- assets/js/tim-ta.js
+++ assets/js/tim-ta.js
@@ -95,7 +95,7 @@ function setColorCode(scheme, key) {
 }
 
 // Test if tcm-common-code.js variable is visible:
-// console.log("var tcmButtonClassNdx:", tcmButtonClassNdx)
+console.log("var objTcmVisById:", objTcmVisById)
 
 var scrTimeout, scrWidth, scrSmall, scrMedium, scrLarge
 
