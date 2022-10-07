893b79519f79c000573ecdcaa3abee7d4ab3d871
pippim <92641463+pippim@users.noreply.github.com>
2022-10-07 05:43:24 -0600

    Break out from setRootColors.js
    So all JavaScript files can share the same code

---
+++ _includes/getRootColors.js
@@ -0,0 +1,103 @@
+---
+---
+/*  /_includes/setRootColors - get Root Colors before:
+        /asset/css/style.scss in all /_layouts
+        test.md
+        tcm-common-code.js
+*/
+
+console.log("Start of /_includes/getRootColors.js")
+// Color Schemes - See below for descriptions
+var colorSchemeCayman = {
+    name: "colorSchemeCayman",
+    "--nav-button-bg-color": "#F0FFF0",
+    "--nav-button-color": "#159957",
+    "--msgq-error-bg-color": "#f44336",
+    "--msgq-warning-bg-color": "#ff9800",
+    "--msgq-info-bg-color": "#2196F3",
+    "--msgq-success-bg-color": "#04AA6D",
+    "--msgq-body-bg-color": "#f1f1f1",
+    "--msgq-border-color": "#d3d3d3",
+    "--header-accent-color": "#FFFF00",
+    "--button-focus-color": "#1E90FF",
+    "--progress-bar-color": "#008000",
+    "--flash-bg-color": "#808080",
+    "--boldest-color": "#000000",
+    "--link-visited": "#800080",
+    "--header-heading-color": "#ffffff",
+    "--header-bg-color": "#159957",
+    "--header-bg-color-secondary": "#155799",
+    "--section-headings-color": "#159957",
+    "--body-text-color": "#606c71",
+    "--body-bg-color": "#ffffff",
+    "--body-link-color": "#1e6bb8",
+    "--body-link-inverted-color": "#e19447",
+    "--blockquote-text-color": "#819198",
+    "--code-bg-color": "#f3f6fa",
+    "--code-text-color": "#567482",
+    "--border-color": "#dce6f0",
+    "--table-border-color": "#e9ebec",
+    "--hr-border-color": "#eff0f1",
+    "--kbd-background-color": "#fafbfc",
+    "--kbd-border-color": "#c6cbd1",
+    "--kbd-border-bottom-color": "#959da5",
+    "--kbd-box-shadow-color": "#959da5",
+    "--kbd-color": "#444d56"
+}
+
+var colorSchemeDark = {
+    name: "colorSchemeDark",
+    "--nav-button-bg-color": "#494b4c",
+    "--nav-button-color": "#f9fbfc",
+    "--msgq-error-bg-color": "#f44336",
+    "--msgq-warning-bg-color": "#ff9800",
+    "--msgq-info-bg-color": "#2196F3",
+    "--msgq-success-bg-color": "#04AA6D",
+    "--msgq-body-bg-color": "#313131",
+    "--msgq-border-color": "#434343",
+    "--header-accent-color": "#FFFF00",
+    "--button-focus-color": "#1E90FF",
+    "--progress-bar-color": "#008000",
+    "--flash-bg-color": "#808080",
+    "--boldest-color": "#ffffff",
+    "--link-visited": "#800080",
+    "--header-heading-color": "#ffd080",
+    "--header-bg-color": "#057937",
+    "--header-bg-color-secondary": "#053779",
+    "--section-headings-color": "#159957",
+    "--body-text-color": "#e0ece1",
+    "--body-bg-color": "#101010",
+    "--body-link-color": "#5eabf8",
+    "--body-link-inverted-color": "#e19447",
+    "--blockquote-text-color": "#819198",
+    "--code-bg-color": "#13161a",
+    "--code-text-color": "#b6b4b2",
+    "--border-color": "#4c4640",
+    "--table-border-color": "#494b4c",
+    "--hr-border-color": "#3f3031",
+    "--kbd-background-color": "#2a2b2c",
+    "--kbd-border-color": "#c6cbd1",
+    "--kbd-border-bottom-color": "#e5ede5",
+    "--kbd-box-shadow-color": "#e5ede5",
+    "--kbd-color": "#040d06"
+}
+
+var currentColorScheme  // Object with all root keys
+
+function getCurrentColors() {
+    /*  Local storage key colorScheme contains our scheme name.
+        If it doesn't exist use Cayman Theme and save to new key.
+    */
+    if (typeof localStorage.getItem("colorScheme") === 'undefined')
+        localStorage.setItem("colorScheme", JSON.stringify(colorSchemeCayman))
+    currentColorScheme = JSON.parse(localStorage.getItem('colorScheme'))
+    return (extractRootColors(currentColorScheme))
+}
+
+getCurrentColors()
+
+/* Long long comments
+
+*/
+
+/* End of /_includes/getRootColors.js */
