fa369798a0ee23322e2c854ee3e9194d2d6ca13e
pippim <92641463+pippim@users.noreply.github.com>
2022-10-23 10:08:01 -0600

    Change to "status" //req.addEventListener("load"

--- assets/js/hyperlinkRecipe.js
+++ assets/js/hyperlinkRecipe.js
@@ -193,7 +193,8 @@ export function processHyperlinkRecipe(id) {
     // TEST URL
     // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
     const req = new XMLHttpRequest()
-    req.addEventListener("load", reqListener)
+    //req.addEventListener("load", reqListener)
+    req.addEventListener("status", reqListener)
     req.open("GET", "http://www.example.org/example.txt")
     req.send()
 
