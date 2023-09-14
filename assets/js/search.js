b2726845cd0b91f596ec00c233087b6018fe0bf4
pippim.com <92641463+pippim@users.noreply.github.com>
2023-09-13 19:52:51 -0600

    Missing ()

--- assets/js/search.js
+++ assets/js/search.js
@@ -343,7 +343,7 @@ jb.onclick = function (event) {
         const headers_array = []
         var headers = document.querySelectorAll("h1,h2,h3,h4")
         for (hj=0; hj<headers.length; hj++) {
-            if !hj.startsWith(' id="', 3) { continue }
+            if (!hj.startsWith(' id="', 3)) { continue }
             if (hj<10) { console.log("Adding header:", headers[hj]) }
             headers_array.push(headers[hj])
         }
