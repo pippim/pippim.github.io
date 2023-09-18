189b228d35daf197a64d6db15af84df6e30b441e
pippim.com <92641463+pippim@users.noreply.github.com>
2023-09-18 10:19:10 -0600

    cayman: "--code-bg-color": "#aa8866"

--- _includes/getRootColors.js
+++ _includes/getRootColors.js
@@ -40,7 +40,7 @@ var colorSchemeCayman = {
     "--body-link-inverted-color": "#e19447",
     "--link-visited": "#800080",
     "--blockquote-text-color": "#819198",
-    "--code-bg-color": "#885577",
+    "--code-bg-color": "#aa8866",
     "--code-text-color": "#29ee14",
     "--border-color": "#dce6f0",
     "--table-border-color": "#e9ebec",
