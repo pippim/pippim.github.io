4a6b48b1274c603ecda4439b42a208ef5b708361
pippim.com <92641463+pippim@users.noreply.github.com>
2023-09-25 15:23:02 -0600

    theCookieJar.js - Update youPlaylistMoveCSV notes

--- assets/js/theCookieJar.js
+++ assets/js/theCookieJar.js
@@ -56,20 +56,54 @@ export youPlaylistAllowPasting() {
 
 export youPlaylistScroll() {
     // https://responsive-muse.com/export-youtube-playlist-video-urls-titles-js/
+    // TODO:  BUTTON 1 copy to clipboard
     let goToBottom = setInterval(() => window.scrollBy(0, 400), 1000)
 }
 
+/*
+
+    <a id="video-title"
+        class="yt-simple-endpoint style-scope ytd-playlist-video-renderer"
+        href="/watch?v=yTje24TS9hw&amp;list=PLthF248A1c6_uYBAqykk-uh9k_D1ba2vA&amp;index=1&amp;pp=iAQB8AUB"
+        title="Slipknot - The Devil In I (Ben Smith Remix) (Official Audio)"
+        > Slipknot - The Devil In I (Ben Smith Remix) (Official Audio)
+    </a>
+
+    <span id="text"
+        class="style-scope ytd-thumbnail-overlay-time-status-renderer"
+        aria-label="4 minutes, 9 seconds">4:09
+    </span>
+*/
+
 export function youPlaylistCopy {
     // https://responsive-muse.com/export-youtube-playlist-video-urls-titles-js/
+    // TODO:  BUTTON 2 copy to clipboard
     clearInterval(goToBottom)
-    let arrayVideos = []
     console.log('\n'.repeat(50))
+
+    let arrayVideos = []
     const links = document.querySelectorAll('a')
     for (const link of links) {
         if (link.id === "video-title") {
             link.href = link.href.split('&list=')[0]
             arrayVideos.push(link.title + ';' + link.href)
-            console.log(link.title + '\t' + link.href)
+        }
+    }
+
+    let arrayTime = []
+    const spans = document.querySelectorAll('span')
+    for (const span of spans) {
+        if (span.id === "text" &&
+        span.classList.contains('ytd-thumbnail-overlay-time-status-renderer'))
+        {
+            arrayTime.push(span.innerText.replace(/[^\d:]/g, ''))
+        }
+    }
+
+    if (arrayVideos.length === arrayTime.length) {
+        for (var i=0; i<arrayVideos.length; i++) {
+            arrayVideos[i] = arrayVideos[i] + ';' + arrayTime[i]
+            console.log(arrayVideos[i])
         }
     }
 }
@@ -84,8 +118,11 @@ Thumbnail 480x360 HQ image is:
 */
 
 
-export function youPlaylistPaste {
+export function youPlaylistSave {
     // https://responsive-muse.com/export-youtube-playlist-video-urls-titles-js/
+    // TODO:  BUTTON 3 copy to clipboard
+    //        Move ~/Downloads/my_data.csv
+    //        To:  ~/.../mserve/YouTubePlaylists/<PLAYLIST NAME>.csv
     let data = arrayVideos.join('\n')
     let blob = new Blob([data], {type: 'text/csv'})
     let elem = window.document.createElement('a')
@@ -95,6 +132,49 @@ export function youPlaylistPaste {
     elem.click()
     document.body.removeChild(elem)
 }
+
+/*  TODO
+export function youPlaylistMoveCSV {
+    //        Move ~/Downloads/my_data.csv
+    //        To:  ~/.../mserve/YouTubePlaylists/<PLAYLIST NAME>.csv
+
+
+        TODO: Mount Window to import playlist
+              See: ~/python/mserve.py website_play()
+```shell
+#!/bin/bash
+#  https://www.pippim.com/programs/mserve.html#pickled-youtube-playlists
+
+#    Follow instructions and note "Copy Button" below:
+
+#        STEP 1: Use CTRL+I in web browser
+#        STEP 2: Click Button 1 to copy to clipboard "youPlayListScroll()"
+#        STEP 3: Go to web browser and use CTRL+V then Enter
+#        STEP 3A: Type "allow pasting" (without the quotes) if requested by browser
+#        STEP 3B: Wait for web browser to stop scrolling, 1 second per song
+#        STEP 4: Click Button 2 to copy to clipboard "youPlaylistCopy()"
+#        STEP 5: Go to web browser and use Ctrl+V then Enter
+#        STEP 6: Click Button 3 to copy to clipboard "youPlaylistSave()"
+#        STEP 7: Go to web browser and use Ctrl+V then Enter
+#        STEP 8: Run this bash script youPlaylistMoveCSV.sh
+#        STEP 9: Use "View Playlists", select Playlist, View Button
+
+if [ "$#" -ne 1 ]; then
+    printf 'ERROR! You must provide the "Playlist Name" in quotes!\n' >&2
+    exit 1
+fi
+
+if [ ! -f ~/Downloads/my_data.csv ]; then
+    printf "ERROR! File ~/Downloads/my_data.csv not found!\n" >&2
+    exit 1
+fi
+
+cd ~/Downloads
+mv -v my_data.csv "$1".csv
+cp -v "$1".csv ~/.local/share/mserve/YouTubePlaylists
+rm -v ~/.local/share/mserve/YouTubePlaylists/"$1".pickle
+```
+*/
 
 // Tip of the Iceberg: https://stackoverflow.com/a/51853700/6929343
 window.addEventListener('beforeunload', () => {
